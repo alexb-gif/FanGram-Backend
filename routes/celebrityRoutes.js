afe91bf0a06eb2b424a3e294060763c085e3a286
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-11-17 17:29:35 +0500

    get celeb

--- routes/celebrityRoutes.js
+++ routes/celebrityRoutes.js
@@ -1,13 +1,33 @@
 const CelebrityController = require("../controllers/celebrityController");
-const { authorizeAdmin, isAuthenticatedUser } = require("../utils/authMiddlewares");
+const {
+  authorizeAdmin,
+  isAuthenticatedUser,
+} = require("../utils/authMiddlewares");
 
 const router = require("express").Router();
 
-router.get('/api/celebrity/search/', CelebrityController.searchCelebrities);
-router.get('/api/celebrity/details/:id', CelebrityController.getCelebrityDetails);
-router.get("/api/celebrity/getAllFeaturedCelebrities", CelebrityController.getAllFeaturedCelebrities);
-router.get("/api/celebrity/getAllCelebrities", CelebrityController.getAllCelebrities);
-router.post("/api/celebrity/addNewCelebrity", isAuthenticatedUser, authorizeAdmin, CelebrityController.addNewCelebrity);
-router.get("/api/celebrity/getCelebritiesByCategories", CelebrityController.getCelebritiesWithSameCategories);
+router.get("/api/celebrity/search/", CelebrityController.searchCelebrities);
+router.get(
+  "/api/celebrity/details/:id",
+  CelebrityController.getCelebrityDetails
+);
+router.get(
+  "/api/celebrity/getAllFeaturedCelebrities",
+  CelebrityController.getAllFeaturedCelebrities
+);
+router.get(
+  "/api/celebrity/getAllCelebrities",
+  CelebrityController.getAllCelebrities
+);
+router.post(
+  "/api/celebrity/addNewCelebrity",
+  isAuthenticatedUser,
+  authorizeAdmin,
+  CelebrityController.addNewCelebrity
+);
+router.post(
+  "/api/celebrity/getCelebritiesByCategories",
+  CelebrityController.getCelebritiesWithSameCategories
+);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,32 +6,29 @@ const {
   addFavorite,
   getFavoriteCelebrities,
   getUserById,
-  
 } = require("../controllers/userController");
-const apiUrl = require("../utils/baseUrl")
-
-
-
+const apiUrl = require("../utils/baseUrl");
 
 const router = require("express").Router();
 
 // Google Auth Routes
-router.get('/auth/google', passport.authenticate('google', { scope: ['profile', "email"] }));
-router.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/' }),
+router.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+router.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
-  res.redirect(apiUrl);
+    res.redirect(apiUrl);
   }
 );
 
-
-
-
-
 router.post("/api/user/register", register);
 router.post("/api/user/login", login);
-router.get("/api/user/details/:id", getUserById)
-router.put("/api/user/update/:id", updateUser)
-router.put("/api/user/addFavorite/:id", addFavorite)
-router.get('/api/user/favoriteCelebrities/:userId', getFavoriteCelebrities);
+router.get("/api/user/details/:id", getUserById);
+router.put("/api/user/update/:id", updateUser);
+router.put("/api/user/addFavorite/:id", addFavorite);
+router.get("/api/user/favoriteCelebrities/:userId", getFavoriteCelebrities);
 
 module.exports = router;
