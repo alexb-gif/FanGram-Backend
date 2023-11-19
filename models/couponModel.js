d7524018d36785e1bdc386fdd3036ae1bd2bfc6a
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-11-19 22:52:11 +0500

    soupon model updated

--- models/couponModel.js
+++ models/couponModel.js
@@ -4,7 +4,6 @@ const couponSchema = new mongoose.Schema({
   name: { type: String, required: true },
   code: { type: String, required: true },
   priceOff: { type: Number },
-
   givenTo: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
 });
 
